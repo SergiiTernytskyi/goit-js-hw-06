4d7a919a37d2f1e58f3cea7ea3ec1247a106ef35
Sergii Ternytskyi <serhij86@gmail.com>
2022-09-11 13:27:24 +0300

    js changed

--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,9 @@
 const inputRef = document.querySelector('#font-size-control');
 const textRef = document.querySelector('#text');
 
-inputRef.addEventListener('change', onInputChange);
+inputRef.addEventListener('input', onInputChange);
+textRef.style.fontSize = `${inputRef.value}px`;
 
 function onInputChange(event) {
   textRef.style.fontSize = `${event.currentTarget.value}px`;
-  console.log(event.currentTarget.value);
 }
--- js/task-10.js
+++ js/task-10.js
@@ -5,32 +5,26 @@ function getRandomHexColor() {
 const refs = {
   createBtn: document.querySelector('[data-create]'),
   destroyBtn: document.querySelector('[data-destroy]'),
-  controlsDiv: document.querySelector('#controls'),
   boxesDiv: document.querySelector('#boxes'),
-  input: document.querySelector('input'),
+  input: document.querySelector('#controls input'),
 };
 
-refs.input.addEventListener('blur', boxQuantity);
+refs.createBtn.addEventListener('blur', boxQuantity);
 refs.createBtn.addEventListener('click', createBoxes);
 refs.destroyBtn.addEventListener('click', destroyBoxes);
 
-const amountArr = [];
 function boxQuantity(event) {
-  console.log(Number(event.currentTarget.value));
-  amountArr.push(Number(event.currentTarget.value));
-  // return Number(event.currentTarget.value);
+  return Number(event.currentTarget.value);
 }
 
-const amount = amountArr[0];
-console.log(amountArr);
-console.log(amount);
+console.log(boxQuantity());
 
 function createBoxes(amount) {
   const boxes = [];
   let width = 30;
   let height = 30;
 
-  for (let i = 1; i <= 20; i += 1) {
+  for (let i = 1; i <= amount; i += 1) {
     width += 10;
     height += 10;
     boxes.push(
